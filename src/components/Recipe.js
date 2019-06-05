6e2c798f1e4a893ee14525d3a65b4b94a2e8934e
Katrinagd <katrinaguyduy@gmail.com>
2019-06-04 20:02:41 -0700

    Created components structure

---
+++ src/components/Recipe.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Recipe extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello from Recipe</h1>
+            </div>
+        )
+    }
+}
---
+++ src/components/RecipeDetails.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { recipe } from '../tempDetails';
+
+export default class RecipeDetails extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello from Details</h1>
+            </div>
+        )
+    }
+}
